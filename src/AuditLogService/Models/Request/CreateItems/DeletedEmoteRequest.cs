06f7352d9bea5db60763dcb6291aabe58e4e3ae6
Michal Halabica <m.halabica@gmail.com>
2023-07-13 18:55:51 +0200

    Deleted length validation in the DeletedEmote event

--- src/AuditLogService/Models/Request/CreateItems/DeletedEmoteRequest.cs
+++ src/AuditLogService/Models/Request/CreateItems/DeletedEmoteRequest.cs
@@ -5,6 +5,5 @@ namespace AuditLogService.Models.Request.CreateItems;
 public class DeletedEmoteRequest
 {
     [Required]
-    [StringLength(32)]
     public string EmoteId { get; set; } = null!;
 }
