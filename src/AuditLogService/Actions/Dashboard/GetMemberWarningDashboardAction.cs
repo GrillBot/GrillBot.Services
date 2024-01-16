662c5e26a27f98e8d2d8ddf4393401a7c78386af
Michal Halabica <m.halabica@gmail.com>
2024-01-16 21:34:43 +0100

    Map CreatedAt property to the Dashboard info row for MemberWarning type

--- src/AuditLogService/Actions/Dashboard/GetMemberWarningDashboardAction.cs
+++ src/AuditLogService/Actions/Dashboard/GetMemberWarningDashboardAction.cs
@@ -4,22 +4,23 @@ using System.Linq.Expressions;
 
 namespace AuditLogService.Actions.Dashboard;
 
-public class GetMemberWarningDashboardAction : DashboardListBaseAction<Core.Entity.MemberWarning>
+public class GetMemberWarningDashboardAction : DashboardListBaseAction<MemberWarning>
 {
     public GetMemberWarningDashboardAction(AuditLogServiceContext context) : base(context)
     {
     }
 
-    protected override Expression<Func<Core.Entity.MemberWarning, bool>>? CreateFilter() => null;
+    protected override Expression<Func<MemberWarning, bool>>? CreateFilter() => null;
 
-    protected override Expression<Func<Core.Entity.MemberWarning, DashboardInfoRow>> CreateProjection()
+    protected override Expression<Func<MemberWarning, DashboardInfoRow>> CreateProjection()
     {
         return entity => new DashboardInfoRow
         {
-            Name = entity.TargetId
+            Name = entity.TargetId,
+            Result = entity.LogItem.CreatedAt.ToString("o")
         };
     }
 
-    protected override Expression<Func<Core.Entity.MemberWarning, DateTime>> CreateSorting()
+    protected override Expression<Func<MemberWarning, DateTime>> CreateSorting()
         => entity => entity.LogItem.CreatedAt;
 }
