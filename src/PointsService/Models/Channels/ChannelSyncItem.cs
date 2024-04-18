5987b78068204510826428aa2451d2532bc6ff61
Michal Halabica <m.halabica@gmail.com>
2024-04-18 21:56:02 +0200

    Added constructors into Points synchronization models

--- src/PointsService/Models/Channels/ChannelSyncItem.cs
+++ src/PointsService/Models/Channels/ChannelSyncItem.cs
@@ -5,4 +5,15 @@ public class ChannelSyncItem
     public string Id { get; set; } = null!;
     public bool? IsDeleted { get; set; }
     public bool? PointsDisabled { get; set; }
+
+    public ChannelSyncItem()
+    {
+    }
+
+    public ChannelSyncItem(string id, bool? isDeleted, bool? pointsDeactivated)
+    {
+        Id = id;
+        IsDeleted = isDeleted;
+        PointsDisabled = pointsDeactivated;
+    }
 }
