7237b3b49b8c5f6b297831340a0eb92f86372a54
Michal Halabica <m.halabica@gmail.com>
2024-04-24 21:27:16 +0200

    Unique index for Unverifies in the UserMeasures

--- src/UserMeasuresService/Core/Entity/UnverifyItem.cs
+++ src/UserMeasuresService/Core/Entity/UnverifyItem.cs
@@ -1,5 +1,8 @@
-﻿namespace UserMeasuresService.Core.Entity;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace UserMeasuresService.Core.Entity;
+
+[Index(nameof(LogSetId), IsUnique = true)]
 public class UnverifyItem : UserMeasureBase
 {
     public DateTime ValidTo { get; set; }
