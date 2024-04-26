c2ac5b4db210daae0b78d350f26dcdbdcc103a9a
Michal Halabica <m.halabica@gmail.com>
2024-04-26 08:09:21 +0200

    Fixed DeleteMeasuresRequest model

--- src/UserMeasuresService/Models/Measures/DeleteMeasuresRequest.cs
+++ src/UserMeasuresService/Models/Measures/DeleteMeasuresRequest.cs
@@ -6,16 +6,19 @@ public class DeleteMeasuresRequest : IValidatableObject
 {
     public Guid? Id { get; set; }
     public long? ExternalId { get; set; }
+
+    [StringLength(64)]
     public string? ExternalIdType { get; set; }
 
     public DeleteMeasuresRequest()
     {
     }
 
-    public DeleteMeasuresRequest(Guid? id, long? externalId)
+    public DeleteMeasuresRequest(Guid? id, long? externalId, string? externalIdType)
     {
         Id = id;
         ExternalId = externalId;
+        ExternalIdType = externalIdType;
     }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
@@ -29,4 +32,7 @@ public class DeleteMeasuresRequest : IValidatableObject
         if (ExternalId is not null && string.IsNullOrEmpty(ExternalIdType))
             yield return new ValidationResult("Required ExternalIdType when ExternalId is filled.");
     }
+
+    public static DeleteMeasuresRequest FromInternalId(Guid id) => new(id, null, null);
+    public static DeleteMeasuresRequest FromExternalSystem(long externalId, string externalIdType) => new(null, externalId, externalIdType);
 }
