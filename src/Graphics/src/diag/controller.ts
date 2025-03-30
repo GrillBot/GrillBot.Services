f0d03c7a534bd4c1afe7b870ca737118b73bd0cf
Michal Halabica <m.halabica@gmail.com>
2025-03-30 21:11:55 +0200

    Fixed uptime

--- src/Graphics/src/diag/controller.ts
+++ src/Graphics/src/diag/controller.ts
@@ -4,7 +4,7 @@ import { stats } from '../common';
 export const onRequest = (request: express.Request, response: express.Response) => {
     response.status(200).json({
         usedMemory: process.memoryUsage().rss,
-        uptime: Math.floor(process.uptime() * 1000),
+        uptime: Math.floor(process.uptime()),
         databaseStatistics: null,
         operations: [],
         ...stats
