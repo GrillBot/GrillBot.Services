697532881771e0738694914e1f6719b039dea585
Michal Halabica <m.halabica@gmail.com>
2025-04-02 09:05:06 +0200

    Created REST endpoint to list cached endpoints

---
+++ src/InviteService/Controllers/InviteController.cs
@@ -0,0 +1,15 @@
+﻿using GrillBot.Core.Models.Pagination;
+using InviteService.Actions;
+using InviteService.Models.Request;
+using InviteService.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InviteService.Controllers;
+
+public class InviteController(IServiceProvider serviceProvider) : GrillBot.Core.Infrastructure.Actions.ControllerBase(serviceProvider)
+{
+    [HttpPost("cached/list")]
+    [ProducesResponseType<PaginatedResponse<Invite>>(StatusCodes.Status200OK)]
+    public Task<IActionResult> GetCachedInvitesAsync([FromBody] InviteListRequest request)
+        => ProcessAsync<GetCachedInvitesAction>(request);
+}
