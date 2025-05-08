d17861c9cf7563acbc72c9ba0ab72f0685822898
Michal Halabica <m.halabica@gmail.com>
2025-05-08 13:44:51 +0200

    Rabbit handler to cancel vote

---
+++ src/EmoteService/Handlers/Suggestions/EmoteSuggestionCancelVoteHandler.cs
@@ -0,0 +1,49 @@
+﻿using EmoteService.Core.Entity;
+using EmoteService.Models.Events.Suggestions;
+using GrillBot.Core.Infrastructure.Auth;
+using GrillBot.Core.Managers.Performance;
+using GrillBot.Core.RabbitMQ.V2.Consumer;
+using GrillBot.Core.RabbitMQ.V2.Publisher;
+using GrillBot.Core.Services.GrillBot.Models.Events.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmoteService.Handlers.Suggestions;
+
+public class EmoteSuggestionCancelVoteHandler(
+    ILoggerFactory loggerFactory,
+    EmoteServiceContext dbContext,
+    ICounterManager counterManager,
+    IRabbitPublisher rabbitPublisher
+) : EmoteSuggestionHandlerBase<EmoteSuggestionCancelVotePayload>(loggerFactory, dbContext, counterManager, rabbitPublisher)
+{
+    protected override async Task<RabbitConsumptionResult> HandleInternalAsync(EmoteSuggestionCancelVotePayload message, ICurrentUserProvider currentUser, Dictionary<string, string> headers)
+    {
+        if (message.SuggestionId == Guid.Empty)
+            return RabbitConsumptionResult.Reject;
+
+        var suggestionQuery = DbContext.EmoteSuggestions
+            .Include(o => o.VoteSession)
+            .Where(o =>
+                o.Id == message.SuggestionId &&
+                o.VoteSession != null &&
+                o.VoteSession.KilledAtUtc == null &&
+                o.VoteSession.ExpectedVoteEndAtUtc >= DateTime.UtcNow
+            );
+
+        var suggestion = await ContextHelper.ReadFirstOrDefaultEntityAsync(suggestionQuery);
+        if (suggestion == null)
+            return RabbitConsumptionResult.Reject;
+
+        var guildQuery = DbContext.Guilds.Where(o => o.GuildId == suggestion.GuildId);
+        var guild = await ContextHelper.ReadFirstOrDefaultEntityAsync(guildQuery);
+        if (guild == null)
+            return RabbitConsumptionResult.Reject;
+
+        suggestion.VoteSession!.KilledAtUtc = DateTime.UtcNow;
+
+        var notificationMessage = CreateAdminChannelNotification(suggestion, guild, suggestion.SuggestionMessageId);
+        await Publisher.PublishAsync((DiscordEditMessagePayload)notificationMessage);
+
+        return RabbitConsumptionResult.Success;
+    }
+}
