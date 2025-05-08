d6926eb2d00a628df77d1712e858dd8525d767be
Michal Halabica <m.halabica@gmail.com>
2025-05-08 10:09:20 +0200

    REST endpoint for getting list of suggestions

---
+++ src/EmoteService/Controllers/EmoteSuggestionsController.cs
@@ -0,0 +1,15 @@
+﻿using EmoteService.Actions.EmoteSuggestions;
+using EmoteService.Models.Request.EmoteSuggestions;
+using EmoteService.Models.Response.EmoteSuggestions;
+using GrillBot.Core.Models.Pagination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmoteService.Controllers;
+
+public class EmoteSuggestionsController(IServiceProvider serviceProvider) : GrillBot.Core.Infrastructure.Actions.ControllerBase(serviceProvider)
+{
+    [HttpPost("list")]
+    [ProducesResponseType<PaginatedResponse<EmoteSuggestionItem>>(StatusCodes.Status200OK)]
+    public Task<IActionResult> GetEmoteSuggestionsListAsync([FromBody] EmoteSuggestionsListRequest request)
+        => ProcessAsync<GetEmoteSuggestionListAction>(request);
+}
---
+++ src/EmoteService/Models/Response/EmoteSuggestions/EmoteSuggestionItem.cs
@@ -0,0 +1,19 @@
+﻿namespace EmoteService.Models.Response.EmoteSuggestions;
+
+public record EmoteSuggestionItem(
+    Guid Id,
+    ulong FromUserId,
+    string Name,
+    DateTime SuggestedAtUtc,
+    ulong GuildId,
+    ulong? NotificationMessageId,
+    bool ApprovedForVote,
+    ulong? ApprovalUserId,
+    DateTime? ApprovedAtUtc,
+    string ReasonToAdd,
+    DateTime? VoteStartAt,
+    DateTime? VoteEndAt,
+    DateTime? VoteKilledAt,
+    int? UpVotes,
+    int? DownVotes
+);
