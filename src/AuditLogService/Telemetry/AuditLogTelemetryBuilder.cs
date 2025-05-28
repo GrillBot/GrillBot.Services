1f87250b9f8743af0d83491b028653998673c95c
Michal Halabica <m.halabica@gmail.com>
2025-05-28 08:28:44 +0200

    Fixed metrics type

--- src/AuditLogService/Telemetry/AuditLogTelemetryBuilder.cs
+++ src/AuditLogService/Telemetry/AuditLogTelemetryBuilder.cs
@@ -15,6 +15,6 @@ public class AuditLogTelemetryBuilder(
         meter.CreateObservableGauge("count_of_files", _telemetryCollector.CountOfFiles.Get, description: "Count of files stored in the audit log");
         meter.CreateObservableGauge("size_of_files", _telemetryCollector.SizeOfFiles.Get, description: "Size of files stored in the audit log");
         meter.CreateObservableGauge("api_avg_durations", _apiTelemetryCollector.GetMeasurements, description: "AVG duration of REST endpoints in the bot.");
-        meter.CreateObservableCounter("jobs_avg_durations", _jobsTelemetryCollector.GetMeasurements, description: "AVG duration of scheduled jobs.");
+        meter.CreateObservableGauge("jobs_avg_durations", _jobsTelemetryCollector.GetMeasurements, description: "AVG duration of scheduled jobs.");
     }
 }
