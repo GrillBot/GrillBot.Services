df4158afeb7d3e20ef6a30b88a8790ab57c655b1
Michal Halabica <m.halabica@gmail.com>
2025-07-09 22:12:06 +0200

    Unveirfy period statistics

---
+++ src/UnverifyService/Actions/Statistics/PeriodStatistics/GetUnverifyPeriodStatistics.cs
@@ -0,0 +1,29 @@
+﻿using GrillBot.Core.Managers.Performance;
+using GrillBot.Services.Common.Actions.Statistics;
+using Microsoft.EntityFrameworkCore;
+using UnverifyService.Core.Entity;
+using UnverifyService.Core.Enums;
+
+namespace UnverifyService.Actions.Statistics.PeriodStatistics;
+
+public class GetUnverifyPeriodStatistics(
+    UnverifyContext dbContext,
+    ICounterManager counterManager
+) : PeriodStatisticsActionBase<UnverifyContext>(dbContext, counterManager)
+{
+    protected override async Task<Dictionary<DateOnly, long>> GetRawDataAsync()
+    {
+        var operationType = GetParameter<UnverifyOperationType>(1);
+
+        var query = DbContext.LogItems.AsNoTracking()
+            .Where(o => o.OperationType == operationType)
+            .GroupBy(o => o.CreatedAt.Date)
+            .Select(o => new
+            {
+                Key = DateOnly.FromDateTime(o.Key),
+                Count = o.LongCount()
+            });
+
+        return await ContextHelper.ReadToDictionaryAsync(query, o => o.Key, o => o.Count);
+    }
+}
---
+++ src/UnverifyService/Controllers/StatisticsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel.DataAnnotations;
+using UnverifyService.Actions.Statistics.PeriodStatistics;
+
+namespace UnverifyService.Controllers;
+
+public class StatisticsController(IServiceProvider serviceProvider) : GrillBot.Core.Infrastructure.Actions.ControllerBase(serviceProvider)
+{
+    [HttpGet("periodStats")]
+    public Task<IActionResult> GetPeriodStatisticsAsync(
+        [Required] string groupingKey,
+        [Required] OperationType operationType
+    ) => ProcessAsync<GetUnverifyPeriodStatistics>(groupingKey, operationType);
+}
