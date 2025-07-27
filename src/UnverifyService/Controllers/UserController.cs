bd6dc4f59efe98ce1a4a12121c5943794a899793
Michal Halabica <m.halabica@gmail.com>
2025-07-27 12:25:23 +0200

    REST endpoint for user modification

---
+++ src/UnverifyService/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using GrillBot.Core.Validation;
+using Microsoft.AspNetCore.Mvc;
+using UnverifyService.Actions.Users;
+using UnverifyService.Models.Request.Users;
+
+namespace UnverifyService.Controllers;
+
+public class UserController(IServiceProvider serviceProvider) : GrillBot.Core.Infrastructure.Actions.ControllerBase(serviceProvider)
+{
+    [HttpPut("{userId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
+    public Task<IActionResult> ModifyUserAsync(
+        [FromRoute, DiscordId] ulong userId,
+        [FromBody] ModifyUserRequest request
+    ) => ProcessAsync<ModifyUserAction>(userId, request);
+}
